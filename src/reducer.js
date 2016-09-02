ccb162f875824717f401c69377acb55bb6eff167
Patrick Vienneau <pvien027@gmail.com>
2016-09-02 14:15:37 -0400

    Initial commit

---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+import * as types from './actionTypes';
+
+const initialState = {
+	count: 0
+};
+
+function reducer(state = initialState, action = {}){
+	switch(action.type){
+		case types.INCREMENT_COUNT:
+			return Object.assign({}, state, {count:state.count+1});
+	}
+
+	return state;
+}
+
+export default reducer;
